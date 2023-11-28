17ada86f195ebc822e306b672ae8dfe259c6e441
Dorkilla <dorkilla58@gmail.com>
2023-11-28 13:48:36 +0100

    design terv

---
+++ src/halallabirintus/tabbedPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package halallabirintus;
+
+/**
+ *
+ * @author dorki
+ */
+class tabbedPane {
+
+    static void setSelected(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
